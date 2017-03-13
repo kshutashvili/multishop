283cecd13cea1df61efb6a8d18bc70d974261980
Dmitry-Kiselev <kdem27@gmail.com>
2017-03-13 21:55:32 +0200

    [TF-2] register user model in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (('Персональная информация'),
+         {'fields': (
+             'first_name', 'last_name', 'email',)}),
+        (
+            ('Права доступа'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                           'groups', 'user_permissions')}),
+        (('Важные даты'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
