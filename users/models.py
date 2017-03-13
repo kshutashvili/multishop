c213f41f73fbf454eb66bcd6e2f551a704f88d95
Dmitry-Kiselev <kdem27@gmail.com>
2017-03-13 15:42:12 +0200

    [TF-2] create User model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,19 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    email = models.EmailField('email address', unique=True, db_index=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __unicode__(self):
+        return self.email
