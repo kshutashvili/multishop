ff006907dfda5a1aa5743e94e8c39d5195dfddf0
Dmitry-Kiselev <kdem27@gmail.com>
2017-03-15 20:12:01 +0200

    [TF-5] add tag to get static files for current site
    template tag, that allows to get staticfiles depending on site
    The SimpleLazyObject wrapper makes sure the DB call only happens when
    the template actually uses the site object.
    This removes the query from the admin pages. It also caches the result
    
    usage example: <script src="{% get_static 'file_name' %}"></script>

---
+++ website/templatetags/custom_tags.py
@@ -0,0 +1,22 @@
+import os
+
+from django import template
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.functional import SimpleLazyObject
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_static(context, filename, *args, **kwargs):
+    """
+    template tag, that allows to get staticfiles depending on site
+    The SimpleLazyObject wrapper makes sure the DB call only happens when
+    the template actually uses the site object.
+    This removes the query from the admin pages. It also caches the result
+    """
+    request = context['request']
+    site = SimpleLazyObject(lambda: get_current_site(request))
+    path = static(site.name + '/' + filename)
+    return path
