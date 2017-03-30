7ba1b58c881c03378df925d19296d471473b3a1f
Dmitry-Kiselev <kdem27@gmail.com>
2017-03-30 20:59:49 +0300

    [TF-22] add recently viewed products

--- shop/catalogue/views.py
+++ shop/catalogue/views.py
@@ -1,6 +1,17 @@
 from oscar.apps.catalogue.views import ProductDetailView as OscarProductDetailView
+from oscar.apps.customer import history
+
 from website.views import SiteTemplateResponseMixin
 
 
 class ProductDetailView(SiteTemplateResponseMixin, OscarProductDetailView):
     template_name = 'detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data()
+        current_product = self.get_object()
+        products = history.get(self.request)
+        if current_product:
+            products = [p for p in products if p != current_product]
+        context['recently_viewed_products'] = products
+        return context
