6fe3f4e36a61f0be5f605f7b9bbf32a0e05ed741
savar19 <3884346@gmail.com>
2017-04-11 12:10:56 +0300

    tf25

--- shop/catalogue/reviews/forms.py
+++ shop/catalogue/reviews/forms.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from django.forms import CheckboxInput
 from django.forms import HiddenInput
 from oscar.apps.catalogue.reviews.forms import ProductReviewForm as \
     CoreProductReviewForm
@@ -12,12 +12,14 @@ class ProductReviewForm(CoreProductReviewForm):
     class Meta:
         model = ProductReview
         fields = ('score', 'body', 'name', 'email', 'site', 'advantage',
-                  'disadvantage')
+                  'disadvantage', 'get_notification')
         widgets = {
             'site': HiddenInput(),
+            'get_notification': CheckboxInput(),
         }
 
     def __init__(self, product, user=None, *args, **kwargs):
         super(ProductReviewForm, self).__init__(product, user, *args, **kwargs)
         self.fields['advantage'].required = False
         self.fields['disadvantage'].required = False
+        self.fields['get_notification'].required = False
