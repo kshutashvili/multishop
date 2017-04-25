8b15d811ad0e066cbba9ce2595188799b2e9dc0f
freundallein <freund.allein@gmail.com>
2017-04-25 22:57:29 +0300

    TF-44 add fuel calculation

--- static/defro/js/estimation.js
+++ static/defro/js/estimation.js
@@ -27,15 +27,45 @@ const estimate_power = () => {
 	} 
 	const system = ($('#t_system').val() === $('#system_new').val()) ? 1 : 1.35
 	const power = ((square*heigth)/30)*lose*system*region
-	const season_power = (power*24*period)/2
+	const season_heat = (power*24*period)/2
 	$('#t_power').html(power.toFixed(1))
-	$('#t_season').html(season_power.toFixed(2))
+	$('#t_season').html(season_heat.toFixed(2))
+}
+
+const calculate_fuel = () => {
+	const season_heat = $('#t_season').html()
+	const wood = (season_heat/4.2/0.7/1000).toFixed(2)
+	$('#wood_volume').html(wood + ' м3')
+	$('#wood_sum').html(((wood/0.8)*$('#wood_cost').val()).toFixed(2))
+	const coal = (season_heat/7.8/1000).toFixed(2)	
+	$('#coal_volume').html(coal + ' т')
+	$('#coal_sum').html(((coal/0.85)*$('#coal_cost').val()).toFixed(2))
+	const browncoal = (season_heat/3.6/1000).toFixed(2) 
+	$('#brown_coal_volume').html(browncoal + ' т')
+	$('#brown_coal_sum').html(((browncoal/0.8)*$('#brown_coal_cost').val()).toFixed(2))
+	const pellets = (season_heat/4.7/1000).toFixed(2)
+	$('#pellets_volume').html(pellets + ' т')
+	$('#pellets_sum').html(((pellets/0.9)*$('#pellets_cost').val()).toFixed(2))
+	const eurobricket = (season_heat/4.9/1000).toFixed(2) 
+	$('#eurobricket_volume').html(eurobricket + ' т')
+	$('#eurobricket_sum').html(((eurobricket/0.85)*$('#eurobricket_cost').val()).toFixed(2))
+	const gas = (season_heat*0.1).toFixed(2)
+	$('#gas_volume').html(gas + ' м3')
+	$('#gas_sum').html(((gas/0.9)*$('#gas_cost').val()).toFixed(2))
+	const electricity = (season_heat*(2/3)).toFixed(2) 
+	$('#electricity_volume').html(electricity + ' кВТ*ч')
+	$('#electricity_sum').html(((electricity/0.99)*$('#electricity_cost').val()).toFixed(2))
+	const diesel = (season_heat/11.9).toFixed(2)
+	$('#diesel_volume').html(diesel + ' л')
+	$('#diesel_sum').html(((coal/0.9)*$('#diesel_cost').val()).toFixed(2))
 }
 
 $(document).ready(function(){
 	estimate_power()
+	calculate_fuel()
 	$('.calculator').on('change', function() {
 		estimate_power()
+		calculate_fuel()
 	});
 })
 
