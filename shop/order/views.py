ddbc1ffb7d5f2e444060d73124f9385304f93b56
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-04 10:34:10 +0300

    [TF-36] minor improvements

--- shop/order/views.py
+++ shop/order/views.py
@@ -3,7 +3,7 @@
 import os
 
 from django.contrib.sites.shortcuts import get_current_site
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 
 from website.views import SiteTemplateResponseMixin
@@ -16,12 +16,15 @@ class SimpleOrderView(SiteTemplateResponseMixin, CreateView):
     success_template_name = 'checkout/order_success.html'
 
     def form_valid(self, form):
-        instance = form.save(commit=False)
-        instance.basket = self.request.basket
-        instance.save()
-        self.request.basket.submit()
-        return render(self.request, self.get_success_template_name(),
-                      {'order': instance})
+        if self.request.basket.num_lines:
+            instance = form.save(commit=False)
+            instance.basket = self.request.basket
+            instance.save()
+            self.request.basket.submit()
+            return render(self.request, self.get_success_template_name(),
+                          {'order': instance})
+        else:
+            return redirect('/')
 
     def get_success_template_name(self):
         template = get_current_site(self.request).config.template
