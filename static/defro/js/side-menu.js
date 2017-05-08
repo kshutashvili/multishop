daf675329739674f24de65fd489a6148b21a6a3f
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-08 22:30:21 +0300

    [TF-55] remove dim

--- static/defro/js/side-menu.js
+++ static/defro/js/side-menu.js
@@ -15,13 +15,9 @@ $('a.dropdown-toggle').click(function () {
 $('.left_side').hover(function () {
     clearTimeout(timeout);
     $('div.collapse').addClass('in');
-    $("body").append("<div id='overlay_on_side_menu_hover'></div>");
-    $('#overlay_on_side_menu_hover').show().css({'filter': 'alpha(opacity=40)'});
-    $('.left_side').css('z-index', 9999999);
 }, function () {
     timeout = setTimeout(function () {
         $('div.collapse').removeClass('in');
-        $('#overlay_on_side_menu_hover').hide();
     }, 500);
 
 });
