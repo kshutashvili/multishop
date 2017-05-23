2f70cca90717a4129976639609744914449ada15
Dmitry-Kiselev <kdem27@gmail.com>
2017-05-23 20:21:57 +0300

    [TF-74] fix orders in dashboard

--- shop/order/views.py
+++ shop/order/views.py
@@ -5,30 +5,37 @@ import os
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
-from oscar.apps.order.utils import OrderNumberGenerator
+from django.views.generic import FormView, CreateView
 
 from website.views import SiteTemplateResponseMixin
 from .forms import OrderForm, CallRequestForm
+from .utils import OrderCreator
 
 
-class SimpleOrderView(SiteTemplateResponseMixin, CreateView):
+class SimpleOrderView(SiteTemplateResponseMixin, FormView):
     form_class = OrderForm
     template_name = 'checkout/order.html'
     success_template_name = 'checkout/order_success.html'
 
     def form_valid(self, form):
         if self.request.basket.num_lines:
-            instance = form.save(commit=False)
-            instance.basket = self.request.basket
-            instance.user = self.request.user
-            instance.number = OrderNumberGenerator().order_number(
-                instance.basket)
-            instance.site = get_current_site(self.request)
-            instance.save()
             self.request.basket.submit()
+            order = OrderCreator().place_order(self.request.basket,
+                                               self.request.basket.total_excl_tax,
+                                               form.cleaned_data[
+                                                   'shipping_method'],
+                                               form.cleaned_data[
+                                                   'shipping_method'].shipping_price,
+                                               user=self.request.user,
+                                               request=self.request)
+            order.name = form.cleaned_data['name']
+            order.payment_method = form.cleaned_data['payment_method']
+            order.city = form.cleaned_data['city']
+            order.phone = form.cleaned_data['phone']
+            order.comment = form.cleaned_data['comment']
+            order.save()
             return render(self.request, self.get_success_template_name(),
-                          {'order': instance})
+                          {'order': order})
         else:
             return redirect('/')
 
