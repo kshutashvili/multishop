ffe5e984a2ba099b1960a122940742d812dcc306
Pavel Koryakin <xtr72@yandex.ru>
2017-05-25 15:22:11 +0300

    TF-95 Fix Product.get_absolute_url

--- shop/catalogue/models.py
+++ shop/catalogue/models.py
@@ -77,8 +77,11 @@ class Product(AbstractProduct):
             return Product.redis
 
     def get_absolute_url(self):
-        cat = self.categories.all().first()
-        slug = cat._slug_separator.join((cat.slug, self.slug))
+        if self.categories.all().exists():
+            cat = self.categories.all().first()
+            slug = cat._slug_separator.join((cat.slug, self.slug))
+        else:
+            slug = self.slug
         return reverse('catalogue:product_or_category',
                        kwargs={'slug': slug})
 
