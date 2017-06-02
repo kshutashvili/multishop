700e3b0184adf50ae0e27e4f46523cc19bf236bc
anton <phonxis@gmail.com>
2017-06-02 17:27:20 +0300

    part of TF-117

--- static/defro/js/more-button.js
+++ static/defro/js/more-button.js
@@ -1,89 +1,72 @@
-$(document).ready(function () {
-    var h = 300, t = $('#tovar_desc'), max = t[0].scrollHeight, min = 0,
-        scoreA = 0;
-    $('.read-next').on('click', function (event) {
-        var H = t.height();
+ $(document).ready(function ()
+	  {  var h = 300, t = $('#tovar_desc'), max = t[0].scrollHeight, min = 0, scoreA = 0;
+	   $('.read-next').on('click', function (event)
+	  {
+	   var H = t.height();
+	   if(scoreA == 0){
+	   	$('.read-next').addClass("read-next-arrow");
+	   	scoreA = 1;
+	   }else{
+	   	$('.read-next').removeClass("read-next-arrow");
+	   	scoreA = 0;
+	   }
 
-        var tables = $('#tovar_desc table');
-        var tables_height = 0;
-        for (var i = 0; i < tables.length; i++) {
-            tables_height += tables[i].scrollHeight;
-        }
+	   if(H == max){H = min}
+	   else if (H  + h > max){H = max}
+	   else {H += h};
+	   t.height(H);
+	   $(this).text(H == max ? 'Свернуть' : 'Читать далее')
+	   return false
+	  })
+	  });
 
-        if (scoreA == 0) {
-            $('.read-next').addClass("read-next-arrow");
-            scoreA = 1;
+
+	 $('.read-next-fullcomment').on('click', function(){
+        is_parent = $(this).parent(); 
+        blok = is_parent.find('.comment_range');
+        if(blok.css('max-height') != 'none'){
+           blok.css('max-height','');
+           $(this).text('Свернуть');
+           $(this).addClass("read-next-arrow");
         } else {
-            $('.read-next').removeClass("read-next-arrow");
-            scoreA = 0;
+           blok.css('max-height','522px');    
+           $(this).text('Читать все отзывы');
+           $(this).removeClass("read-next-arrow");
         }
+         
+        return false;
+    });
 
-        if (H == max) {
-            H = min
+
+ 		$('.read-next-comment').on('click', function(){
+        is_parent = $(this).parent(); 
+        blok = is_parent.find('.user_comment');
+        if(blok.css('max-height') != 'none'){
+           blok.css('max-height','');
+           $(this).text('Свернуть');
+           $(this).addClass("read-next-arrow");
+        } else {
+           blok.css('max-height','53px');    
+           $(this).text('Читать далее');
+           $(this).removeClass("read-next-arrow");
         }
-        else if (H + h > max) {
-            H = max
+         
+        return false;
+    });
+
+     
+    $('.read-next-com').on('click', function(){
+        is_parent = $(this).parent(); 
+        blok = is_parent.find('.user_comment');
+        if(blok.css('max-height') != 'none'){
+           blok.css('max-height','');
+           $(this).text('Свернуть');
+           $(this).addClass("read-next-arrow");
+        } else {
+           blok.css('max-height','50px');    
+           $(this).text('Читать далее');
+           $(this).removeClass("read-next-arrow");
         }
-        else {
-            H += h
-        }
-        ;
-
-        H += tables_height / 1.5;
-
-        t.height(H);
-        $(this).text(H == max ? 'Свернуть' : 'Читать далее')
-        return false
-    })
-});
-
-
-$('.read-next-fullcomment').on('click', function () {
-    is_parent = $(this).parent();
-    blok = is_parent.find('.comment_range');
-    if (blok.css('max-height') != 'none') {
-        blok.css('max-height', '');
-        $(this).text('Свернуть');
-        $(this).addClass("read-next-arrow");
-    } else {
-        blok.css('max-height', '522px');
-        $(this).text('Читать все отзывы');
-        $(this).removeClass("read-next-arrow");
-    }
-
-    return false;
-});
-
-
-$('.read-next-comment').on('click', function () {
-    is_parent = $(this).parent();
-    blok = is_parent.find('.user_comment');
-    if (blok.css('max-height') != 'none') {
-        blok.css('max-height', '');
-        $(this).text('Свернуть');
-        $(this).addClass("read-next-arrow");
-    } else {
-        blok.css('max-height', '53px');
-        $(this).text('Читать далее');
-        $(this).removeClass("read-next-arrow");
-    }
-
-    return false;
-});
-
-
-$('.read-next-com').on('click', function () {
-    is_parent = $(this).parent();
-    blok = is_parent.find('.user_comment');
-    if (blok.css('max-height') != 'none') {
-        blok.css('max-height', '');
-        $(this).text('Свернуть');
-        $(this).addClass("read-next-arrow");
-    } else {
-        blok.css('max-height', '50px');
-        $(this).text('Читать далее');
-        $(this).removeClass("read-next-arrow");
-    }
-
-    return false;
-});
+         
+        return false;
+    });
--- static/defro/js/script-for-catalog.js
+++ static/defro/js/script-for-catalog.js
@@ -1,65 +1,65 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-    $('#myTabs a').click(function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    });
+  $('#myTabs a').click(function (e) {
+	  e.preventDefault()
+	  $(this).tab('show')
+	});
 
 
-    $('.owl-carousel').owlCarousel({
-        items: 3,
-        margin: 15,
-        nav: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            480: {
-                items: 2
-            },
+  $('.owl-carousel').owlCarousel({
+    items:3,
+      margin:15,
+      nav:true, 
+      responsive:{
+          0:{
+              items:1
+          },
+          480:{
+              items:2
+          },
 
-            620: {
-                items: 3
-            },
-            1000: {
-                items: 4
-            },
-            1200: {
-                items: 5
-            }
-        }
-    });
+          620:{
+              items:3
+          },
+          1000:{
+              items:4
+          },
+          1200:{
+              items:5
+          }
+      }
+  });
 
     $('.bxslider_two').bxSlider({
-        pagerCustom: '#bx-pager',
-        minSlides: 1,
-        maxSlides: 1,
-        moveSlides: 1
-    });
-    jQuery('#scrollup img').click(function () {
-        window.scroll(0, 0);
-        return false;
-    });
-
-    jQuery(window).scroll(function () {
-        if (jQuery(document).scrollTop() > 0) {
-            jQuery('#scrollup').fadeIn('fast');
-        } else {
-            jQuery('#scrollup').fadeOut('fast');
-        }
+    pagerCustom: '#bx-pager', 
+    minSlides: 1,
+    maxSlides: 1,
+    moveSlides: 1
+  });
+    jQuery('#scrollup img').click( function(){
+      window.scroll(0 ,0); 
+      return false;
     });
 
-    $(".plitka").click(function () {
-        $(this).addClass("plitka_red");
-        $(".spisok").removeClass("spisok_red");
-        $(".catalog_items").removeClass("catalog_item_list");
+    jQuery(window).scroll(function(){
+      if ( jQuery(document).scrollTop() > 0 ) {
+        jQuery('#scrollup').fadeIn('fast');
+      } else {
+        jQuery('#scrollup').fadeOut('fast');
+      }
     });
-    $(".spisok").click(function () {
-        $(this).addClass("spisok_red");
-        $(".plitka").removeClass("plitka_red");
-        $(".catalog_items").addClass("catalog_item_list");
+
+    $(".plitka").click(function(){
+      $(this).addClass("plitka_red");
+      $(".spisok").removeClass("spisok_red");
+      $(".catalog_items").removeClass("catalog_item_list");
     });
-    $('.filter_media').click(function () {
+    $(".spisok").click(function(){
+      $(this).addClass("spisok_red");
+      $(".plitka").removeClass("plitka_red");
+      $(".catalog_items").addClass("catalog_item_list");
+    });
+ $('.filter_media').click(function () {
         $('.left_filter ').show();
         $('.shadow').show();
     });
@@ -68,14 +68,14 @@ $(document).ready(function () {
     });
     filterMove();
 });
-$(window).resize(function () {
-    filterMove()
-});
-function filterMove() {
-    if ($(window).width() <= 992) {
+$(window).resize(function() {filterMove ()});
+function filterMove () {
+    if($(window).width()<=992)
+    {
         $('.left_filter .parameters').appendTo('.mobile_filter_modal')
     }
-    else {
+    else
+    {
         $('.left_filter .parameters').prependTo('.left_filter')
     }
 }
