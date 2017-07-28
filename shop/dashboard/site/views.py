9f7e22e12d8d7ffde7df841be70826c910468f66
antons <anton.shestakov2@gmail.com>
2017-07-28 16:23:16 +0300

    TF-150 - fixed image upload file

--- shop/dashboard/site/views.py
+++ shop/dashboard/site/views.py
@@ -16,17 +16,17 @@ class SiteCreateView(CreateView):
         context['title'] = _('Create Site')
         if self.request.POST:
             context['site_config_form'] = SiteConfigForm(self.request.POST,
-                                                         instance=kwargs.pop('instance'))
+                                                         self.request.FILES)
         else:
             context['site_config_form'] = SiteConfigForm()
 
         return context
 
     def form_valid(self, form):
-        self.object = form.save()
-
-        context = self.get_context_data(instance=self.object.config)
+        context = self.get_context_data()
         site_config = context['site_config_form']
+        self.object = form.save()
+        site_config.instance = self.object.config
 
         if site_config.is_valid():
             site_config.save()
