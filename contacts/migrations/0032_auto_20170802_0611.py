afb7502c367e7d66ddeaf2a2fe6cac9048851fb7
antons <anton.shestakov2@gmail.com>
2017-08-09 11:43:18 +0300

    contacts migrations restored

--- contacts/migrations/0032_auto_20170802_0611.py
+++ contacts/migrations/0032_auto_20170802_0611.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contacts', '0030_data_migration_slug_field_flatpage'),
+        ('contacts', '0031_removed_translated_slug_field'),
     ]
 
     operations = [
