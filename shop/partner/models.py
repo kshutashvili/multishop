5e4e475df7c4ba2fb6798759889ef1e9a07c0f94
anton <phonxis@gmail.com>
2017-09-03 19:33:27 +0300

    TF-194 changes in model

--- shop/partner/models.py
+++ shop/partner/models.py
@@ -9,6 +9,16 @@ from oscar.apps.partner.abstract_models import AbstractStockRecord
 
 
 class StockRecord(AbstractStockRecord):
+    partner_sku = models.CharField(_("Partner SKU"), max_length=128,
+                                   blank=True, null=True)
+    partner = models.ForeignKey(
+        'partner.Partner',
+        on_delete=models.CASCADE,
+        verbose_name=_("Partner"),
+        related_name='stockrecords',
+        blank=True,
+        null=True)
+
     previous_price = models.DecimalField(
         _("Previous Price"), decimal_places=2, max_digits=12,
         blank=True, null=True)
