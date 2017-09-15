2ed77232c9dd3617c22af35b7e769b1717a09abe
antons <anton.shestakov2@gmail.com>
2017-09-15 11:14:36 +0300

    localized urls fix

--- config/templatetags/url_tags.py
+++ config/templatetags/url_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from urlparse import urlparse
 from django.utils.translation import get_language
+from django.contrib.sites.shortcuts import get_current_site
 from django.conf import settings
 from contacts.models import FlatPage
 
@@ -8,8 +9,8 @@ from contacts.models import FlatPage
 register = template.Library()
 
 
-@register.simple_tag
-def url_format(url):
+@register.simple_tag(takes_context=True)
+def url_format(context, url):
     if url == '#':
         return url
     current_lang = get_language()
@@ -18,10 +19,12 @@ def url_format(url):
 
     if current_lang != default_lang:
         # get localized slug for flat page
+        site = get_current_site(context['request'])
         try:
-            kwargs = {'slug_%s' % default_lang: url}
-            formatted_url = FlatPage.objects.get(**kwargs).slug
-        except FlatPage.DoesNotExist:
+            kwargs = {'slug_%s' % default_lang: url,
+                      'site': site}
+            formatted_url = FlatPage.objects.filter(**kwargs).first().slug
+        except AttributeError:
             pass
 
         return "/%s/%s/" % (current_lang, formatted_url)
